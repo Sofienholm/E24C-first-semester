f8138812b9e4f2f653d86a9d7f34827ef5475136
Per Thykjaer Jensen <per@multimusen.dk>
2025-01-24 11:58:00 +0100

    wip

---
+++ 2-sem/uv-25-01-24/ducket-loesning-hotel/js/hotel-rengoering.js
@@ -0,0 +1,49 @@
+/**
+ * Fil: hotel-rengoering.js
+ * Formål: din personlige digitale servicemedarbejderassistent.
+ * Næste version: serviceassistenten skal kunne klikke ok, når der fx er støvsuget
+ */
+
+/** spørger rengøringsassistenten: er der rent? */
+function rent( tjek ){
+    if (tjek === true){
+        /** HTML "injektion" */
+        instruks.innerHTML = `
+        <h2>Udmærket, du fortsætter bare ...</h2>
+        <div class="whitespace"></div>
+        <label>Gå til næste rum!</label>
+        <div class="whitespace"></div>
+        <button  onclick="naesteRum()">OK</button>`
+    
+        /** CSS: Vise/skjule */
+        erDetRent.style.visibility = "hidden"
+        instruks.style.visibility = "visible"
+    }
+    else {
+        /** HTML "injektion" */
+        instruks.innerHTML = `
+            <h2>Der skal altså gøres rent i værelset</h2>
+            <div class="whitespace"></div>
+            <p>Gør rummet rent nu!</p>
+            <div class="whitespace"></div>
+            <ol>
+                <li>Støvsug</li>
+                <li>Skift sengetøj</li>
+                <li>Gør badeværelset rent</li>
+            </ol>
+            <div class="whitespace"></div>
+            <button id="ja" onclick="rent(true)">Klik her når der er rent.</button>        
+        `
+        
+        /** CSS: Vise/skjule */
+        erDetRent.style.visibility = "hidden"
+        instruks.style.visibility = "visible"
+    }
+}
+
+/** rengøringsassistenten går til det næste rum */
+function naesteRum(){
+    /** CSS: Vise/skjule */
+    erDetRent.style.visibility = "visible"
+    instruks.style.visibility = "hidden"
+}
