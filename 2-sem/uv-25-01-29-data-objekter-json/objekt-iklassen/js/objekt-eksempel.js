1ed2dccc341fdc094edf2f83fb76d48e58b71f8b
Per Thykjaer Jensen <per@multimusen.dk>
2025-01-29 11:23:05 +0100

    kode i klassen

---
+++ 2-sem/uv-25-01-29-data-objekter-json/objekt-iklassen/js/objekt-eksempel.js
@@ -0,0 +1,32 @@
+/**
+ * Fil: objekt-eksempel.js
+ * Formål: demo af objekt
+ */
+
+// objekter
+let person = {
+	id: 23,
+	forNavn: "Brian",
+  efterNavn: "Jensen",
+  fra: "Sønderjylland",
+  billede: "brian.jpeg"
+}
+
+let person2 = {
+  id: 24,
+  forNavn: "Anne",
+  efterNavn: "Pedersen",
+  fra: "Vendsyssel",
+  billede: "anne.jpeg"
+}
+
+// skriv til DOM
+resultat.innerHTML = `
+    <article>
+    <h2> Navn: ${person2.forNavn} ${person2.efterNavn} </h2>
+    <figure>
+        <img src="billeder/${person2.billede}" alt="Foto: ${person2.forNavn} ${person2.efterNavn}">
+        <figcaption> Foto: ${person2.forNavn} ${person2.efterNavn} </figcaption>
+    </figure>
+    </article>
+`
