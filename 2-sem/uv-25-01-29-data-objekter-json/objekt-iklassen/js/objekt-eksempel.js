323130535fbd323ddafca653fadc04576f3c2c4e
Per Thykjaer Jensen <per@multimusen.dk>
2025-01-29 11:52:50 +0100

    js opdateret

--- 2-sem/uv-25-01-29-data-objekter-json/objekt-iklassen/js/objekt-eksempel.js
+++ 2-sem/uv-25-01-29-data-objekter-json/objekt-iklassen/js/objekt-eksempel.js
@@ -6,32 +6,43 @@
 // objekter
 let person = {
 	id: 23,
-	forNavn: "Brian",
-  efterNavn: "Jensen",
-  fra: "Sønderjylland",
-  billede: "brian.jpeg"
+    forNavn: "Brian",
+    efterNavn: "Jensen",
+    fra: "Sønderjylland",
+    billede: "brian.png",
+    hilsen: function(){
+        alert(person.forNavn + " siger hej med dig.")
+    }
 }
 
 let person2 = {
-  id: 24,
-  forNavn: "Anne",
-  efterNavn: "Pedersen",
-  fra: "Vendsyssel",
-  billede: "anne.jpeg",
-  hilsen: function(){
-    alert(this.forNavn + " siger hej med dig.")
-  }
+    id: 24,
+    forNavn: "Anne",
+    efterNavn: "Pedersen",
+    fra: "Vendsyssel",
+    billede: "anne.jpeg",
+    hilsen: function(){
+        alert(person2.forNavn + " siger hej med dig.")
+    }
 }
 
-// skriv til DOM
-resultat.innerHTML = `
+// skriv til DOM via funktion
+function skrivTilDOM( hvem ){
+    resultat.innerHTML += `
     <article>
-    <h2> Navn: ${person2.forNavn} ${person2.efterNavn} </h2>
-    <figure>
-        <!-- event bygges ind i billedet -->
-        <img onclick="person2.hilsen()" src="billeder/${person2.billede}" alt="Foto: ${person2.forNavn} ${person2.efterNavn}">
-        <figcaption> Foto: ${person2.forNavn} ${person2.efterNavn} </figcaption>
-    </figure>
+       <h2> Navn: ${hvem.forNavn} ${hvem.efterNavn} </h2>
+       <figure>
+           <!-- event bygges ind i billedet -->
+           <img onclick="person2.hilsen()" src="billeder/${hvem.billede}" alt="Foto: ${hvem.forNavn} ${hvem.efterNavn}">
+           <figcaption> 
+              Foto: ${hvem.forNavn} ${hvem.efterNavn} 
+           </figcaption>
+        </figure>
     </article>
 `
-//person2.hilsen()
+}
+
+// funktionen kaldes (kræver ensartede objekter)
+skrivTilDOM(person)
+skrivTilDOM(person2)
+
