70bc0f54e1f159ce6f5862bed0b2930c09f2fbd8
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2025-03-21 10:50:16 +0100

    to koort

--- 2-sem/uv-25-03-21-map-leaflet/js/leaflet-noter.js
+++ 2-sem/uv-25-03-21-map-leaflet/js/leaflet-noter.js
@@ -5,7 +5,7 @@
  */
 
 // Kortet centreres på en geografisk position
-const kortCenter = [51.505, -0.09] // London
+const kortCenter = [55.250310, 9.482092] // London
 
 // kortet centreres nu på kortCenter
 var map = L.map('map').setView( kortCenter , 15)
@@ -17,35 +17,34 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map)
 
 // cirkel
-/*
 var circle = L.circle( kortCenter, {
     color: 'yellow',
     fillColor: 'pink',
     fillOpacity: 0.3,
     radius: 222
 }).addTo(map)
-*/
+
 
 // marker
-/*
 var marker = L.marker( kortCenter ).addTo(map)
-*/
+
+// damager kirkegaard
+var damager = L.marker( [55.24, 9.46] ).addTo(map)
+damager.bindPopup("Ude i vandet")
 
 // geometrisk poligon
-/*
 var polygon = L.polygon([
     [51.509, -0.08],
     [51.503, -0.06],
     [51.51, -0.047]
 ]).addTo(map)
-*/
+
 
 // popup'ere forbindes med variablen:
-/*
-marker.bindPopup("<b>Hva' så da!</b><br>Jeg er en popup.").openPopup()
-circle.bindPopup("I am a circle.")
-polygon.bindPopup("I am a polygon.")
-*/
+marker.bindPopup("<b>Hva' så da!</b><br>Jeg er en popup.")
+//circle.bindPopup("I am a circle.")
+//polygon.bindPopup("I am a polygon.")
+
 
 // openPopup() metoden vil åbne et lag oven på kortet
 /*
@@ -55,7 +54,7 @@ polygon.bindPopup("I am a polygon.")
 */
 
 // events kan anvendes sådan:
-/*
+
 function onMapClick(e) {
     alert("You clicked the map at " + e.latlng)
 }
@@ -63,4 +62,7 @@ function onMapClick(e) {
 map.on('click', onMapClick) // viser positionen i en alert
 // hvad kan det her bruges til?
 // kan andre events end click bruges her?
-*/
+
+
+// Haderslev
+// https://www.openstreetmap.org/search?query=haderslev#map=17/55.250310/9.482092
